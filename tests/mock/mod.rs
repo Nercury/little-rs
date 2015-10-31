a91d84eb7bc763fc94b317bbc13a315f5c7b3794
Nerijus Arlauskas <nercury@gmail.com>
2015-10-31 16:54:11 +0200

    Make interpreter tests external.

---
+++ tests/mock/mod.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+
+use little::LittleValue;
+
+/// Simple value implementation.
+#[derive(Clone, Eq, PartialEq, PartialOrd)]
+pub enum Value {
+    Null,
+    Int(i64),
+    Str(String)
+}
+
+impl LittleValue for Value { }
+
+impl Default for Value {
+    fn default() -> Value {
+        Value::Null
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Value::Null => Ok(()),
+            Value::Int(ref i) => write!(f, "{}", i),
+            Value::Str(ref s) => write!(f, "{}", s),
+        }
+    }
+}
